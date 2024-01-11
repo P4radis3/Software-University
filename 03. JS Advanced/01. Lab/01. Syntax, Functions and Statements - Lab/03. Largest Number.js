50ace26eb7981f57508932cd03f97cd50cf29217
P4radis3 <112849433+P4radis3@users.noreply.github.com>
2024-01-11 13:59:30 +0200

    Fixing files plus uploading exercise homework.

--- 03. JS Advanced/01. Lab/01. Syntax, Functions and Statements - Lab/03. Largest Number.js
+++
@@ -1,6 +0,0 @@
-function largestNumber(firstNumber, secondNumber, thirdNumber) {
-
-    let largestNum = Math.max(firstNumber, secondNumber, thirdNumber);
-    console.log(`The largest number is ${largestNum}.`);
-
-}
