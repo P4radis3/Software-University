535f73edfdf4517ff4c824f2649a98f3fb625bc3
Paradise <112849433+P4radis3@users.noreply.github.com>
2024-02-03 13:12:02 +0200

    Modifying files so they work properly in Judge.

--- 03. JS Advanced/01. Lab/08. Classes - Lab/02. Get Persons.js
+++ 03. JS Advanced/01. Lab/08. Classes - Lab/02. Get Persons.js
@@ -1,29 +1,31 @@
-class Person {
+function getPersons() {
+    class Person {
 
-    constructor(firstName, lastName, age, email) {
+        constructor(firstName, lastName, age, email) {
 
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.email = email;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
 
-    }
+        }
 
-    toString() {
+        toString() {
 
-        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
 
-    }
+        }
 
-    static getPersons() {
+        static getPersons() {
 
-        return [
-            
-            new Person('Anna', 'Simpson', 22, 'anna@yahoo.com'),
-            new Person('SoftUni'),
-            new Person('Stephan', 'Johnson', 25),
-            new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com')
+            return [
 
-        ];
+                new Person('Anna', 'Simpson', 22, 'anna@yahoo.com'),
+                new Person('SoftUni'),
+                new Person('Stephan', 'Johnson', 25),
+                new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com')
+
+            ];
+        }
     }
 }
