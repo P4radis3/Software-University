5bb1e995076465f907bbc0f1f7b341c38103d642
Paradise <112849433+P4radis3@users.noreply.github.com>
2024-02-23 20:45:41 +0200

    HTTP and REST Services - Lab & Exercise homework.

---
+++ 04. JS Applications/02. Asynchronous Programming/01.XHR/scripts/app.js
@@ -0,0 +1,14 @@
+function loadGitHubRepository() {
+
+   let xhr = new XMLHttpRequest();
+   xhr.onreadystatechange = function () {
+
+      if (xhr.readyState === 4 && xhr.status === 200) {
+         document.getElementById("res").textContent = xhr.responseText;
+      }
+   };
+
+   xhr.open("GET", "https://api.github.com/users/testnakov/repos", true);
+   xhr.send();
+
+}
