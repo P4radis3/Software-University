34fcc51f24a829690297e8ea9b5e5238abb25a0e
Paradise <112849433+P4radis3@users.noreply.github.com>
2024-05-30 19:54:36 +0300

    More file compatibility.

--- 05. JS Back-End/01. Lab/Cat Shelter/src/util.js
+++ 05. JS Back-End/01. Lab/Cat Shelter/src/util.js
@@ -8,29 +8,30 @@ const searchBar = `
 </form>`;
 
 async function readFile(filePath) {
-
     const fileHandle = await fs.open(path.join('./', filePath), 'r');
     return fileHandle.createReadStream();
-
 }
 
 async function readTemplate(template) {
-
     const data = await fs.readFile(path.join('./views/', template + '.html'));
     return data.toString();
-
 }
 
 async function layout(body, hasSearch) {
-
     let layoutTemplate = await readTemplate('layout');
     let search = '';
 
-    if (hasSearch) { search = searchBar; }
+    if (hasSearch) {
+        search = searchBar;
+    }
 
     layoutTemplate = layoutTemplate.replace('%%searchBar%%', search);
-    return layoutTemplate.replace('%%body%%', body);
 
+    return layoutTemplate.replace('%%body%%', body);
 }
 
-module.exports = { readFile, readTemplate, layout };
+module.exports = {
+    readFile,
+    readTemplate,
+    layout
+};
