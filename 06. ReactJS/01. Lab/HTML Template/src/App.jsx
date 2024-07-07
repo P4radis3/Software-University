ddc2db6c4d48c1bcb8533629eb2a60b436c5583b
Yordan Tabakov <112849433+P4radis3@users.noreply.github.com>
2024-07-07 13:03:04 +0300

    Uploaded ReactJS Homework.

---
+++ 06. ReactJS/01. Lab/HTML Template/src/App.jsx
@@ -0,0 +1,50 @@
+import Navigation from './components/Navigation'
+import Banner from './components/Banner'
+
+import About from './components/About'
+import Video from './components/Video'
+
+import Features from './components/Features'
+import Team from './components/Team'
+
+import Testimonials from './components/Testimonials'
+import FAQ from './components/FAQ'
+
+import Contacts from './components/Contacts'
+import Download from './components/Download'
+
+import Footer from './components/Footer'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Navigation />
+
+      <Banner />
+
+      <About />
+
+      <Video />
+
+      <Features />
+
+      <Team />
+
+      <Testimonials />
+
+      <FAQ />
+
+      <Contacts />
+
+      <Download />
+
+      <Footer />
+
+    </>
+  )
+}
+
+export default App
