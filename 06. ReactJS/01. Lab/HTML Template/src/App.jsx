19b76636026609e23c91e59c814afde712ee1f6e
Yordan Tabakov <112849433+P4radis3@users.noreply.github.com>
2024-07-29 18:07:49 +0300

    Uploaded ReactJS Homework.

--- 06. ReactJS/01. Lab/HTML Template/src/App.jsx
+++ 06. ReactJS/01. Lab/HTML Template/src/App.jsx
@@ -1,50 +1,41 @@
-import Navigation from './components/Navigation'
-import Banner from './components/Banner'
-
-import About from './components/About'
-import Video from './components/Video'
-
-import Features from './components/Features'
-import Team from './components/Team'
-
-import Testimonials from './components/Testimonials'
-import FAQ from './components/FAQ'
-
-import Contacts from './components/Contacts'
-import Download from './components/Download'
-
-import Footer from './components/Footer'
-
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import About from './components/About';
+import WatchNow from './components/WatchNow';
+import Features from './components/Features';
+import OurTeam from './components/OurTeam';
+import Testimonials from './components/Testimonials';
+import FAQ from './components/FAQ';
+import ContactUs from './components/ContactUs';
+import DownloadApp from './components/DownloadApp';
+import Footer from './components/Footer';
 
 function App() {
+    return (
+        <>
+            <Navigation />
 
+            <Header />
 
-  return (
-    <>
-      <Navigation />
+            <About />
 
-      <Banner />
+            <WatchNow />
 
-      <About />
+            <Features />
 
-      <Video />
+            <OurTeam />
 
-      <Features />
+            <Testimonials />
 
-      <Team />
+            <FAQ />
 
-      <Testimonials />
+            <ContactUs />
 
-      <FAQ />
+            <DownloadApp />
 
-      <Contacts />
-
-      <Download />
-
-      <Footer />
-
-    </>
-  )
+            <Footer />
+        </>
+    )
 }
 
 export default App
